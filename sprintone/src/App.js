15d9dae64f212d4d34eb6f215e8e2fbf6092702d
Prashant-KT <tiwariprashant0395@gmail.com>
2022-05-23 15:25:40 +0530

    working on sorting

--- sprintone/src/App.js
+++ sprintone/src/App.js
@@ -5,14 +5,31 @@ import "./App.css";
 import axios from 'axios'
 export default function App() {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  // const [order]
 
   useEffect(() => {
-      axios.get('https://json-server-mocker-masai.herokuapp.com/candidates').then((res)=>{
+      // axios.get('https://json-server-mocker-masai.herokuapp.com/candidates',{
+      axios.get('http://localhost:3001/candidates',{
+        params:{
+          _limit:5,
+          _page:page,
+          _sort:"salary",
+          _order:"ASC"
+        }
+      }).then((res)=>{
       setData(res.data)
       })
 
-  }, [])
+  }, [page])
   
+  const nextPage = () =>{
+    setPage(page+1)
+  }
+
+  const prevPage = () =>{
+    setPage(page-1)
+  }
 
   return (
     <div className="App">
@@ -20,16 +37,15 @@ export default function App() {
         <div id="loading-container">...Loading</div>
 
         <Button id="SORT_BUTTON" title={`Sort by Ascending Salary`} />
-        <Button title="PREV" id="PREV" />
-        <Button id="NEXT" title="NEXT" />
-        {
+        <Button title="PREV" id="PREV"  onClick={prevPage}/>
+        <Button id="NEXT" title="NEXT" onClick={nextPage}/>
+        {/* <button onClick={nextPage}>next</button> */}
+      </div>
+      {
           data.map((item ,ind)=>{
             return <CandidateCard key={ind} {...item} />
           })
         }
-
-      </div>
-      {data.map((item) => {})}
     </div>
   );
 }
